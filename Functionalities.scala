8a92c795ea668631bbee58783cdf82ed9f0a9dfe
Your Name <you@example.com>
2018-04-12 23:58:22 -0400

    Commit1

---
+++ Functionalities.scala
@@ -0,0 +1,40 @@
+import org.apache.log4j.Level
+import java.util.regex.Pattern
+import java.util.regex.Matcher
+
+object Utilities {
+    // Logging error messages to avoid Spam
+  def setupLogging() = {
+    import org.apache.log4j.{Level, Logger}   
+    val rootLogger = Logger.getRootLogger()
+    rootLogger.setLevel(Level.ERROR)   
+  }
+  
+  /** Twitter credentials setup*/
+  def setupTwitter() = {
+    import scala.io.Source
+    
+    for (line <- Source.fromFile("../twitter.txt").getLines) {
+      val fields = line.split(" ")
+      if (fields.length == 2) {
+        System.setProperty("twitter4j.oauth." + fields(0), fields(1))
+      }
+    }
+  }
+  
+  /** Regex pattern retrieval for apachae log accesss */
+  def apacheLogPattern():Pattern = {
+    val ddd = "\\d{1,3}"                      
+    val ip = s"($ddd\\.$ddd\\.$ddd\\.$ddd)?"  
+    val client = "(\\S+)"                     
+    val user = "(\\S+)"
+    val dateTime = "(\\[.+?\\])"              
+    val request = "\"(.*?)\""                 
+    val status = "(\\d{3})"
+    val bytes = "(\\S+)"                     
+    val referer = "\"(.*?)\""
+    val agent = "\"(.*?)\""
+    val regex = s"$ip $client $user $dateTime $request $status $bytes $referer $agent"
+    Pattern.compile(regex)    
+  }
+}
